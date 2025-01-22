3410b1ee3137eee88bc924ac47d06dfbabb626e0
iaschnei <iaschnei@student.42.fr>
2025-01-22 13:08:07 +0100

    Added basic tests

--- main.c
+++ main.c
@@ -1,23 +1,34 @@
 #include "libft_malloc/libft_malloc.h"
+#include "libft/libft.h"
+#include <assert.h>
 int printf(const char *format, ...);
+int strcmp(const char *s1, const char *s2);
 
-// export LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH
+// export LD_LIBRARY_PATH=./libft_malloc:$LD_LIBRARY_PATH
 
 int main(void) {
 
-  int *ptr = (int *)malloc(sizeof(int));
-  if (ptr == NULL) {
-    return (1);
-  }
+  printf("------ SMALL ALLOCS : ------\n");
   
-  *ptr = 42;
-  printf("%d\n", *ptr);
+  int *int_ptr = malloc(sizeof(int));
 
-  int *ptr2 = (int *)malloc(sizeof(int));
-  if (ptr2 == NULL) {
-    return (1);
-  }
+  *int_ptr = 42;
 
-  *ptr = 43;
-  printf("%d\n", *ptr2);
+  assert(*int_ptr==42);
+  printf("int *  -> 42        = %d\n", *int_ptr);
+
+  char *char_ptr = malloc(sizeof(char));
+
+  *char_ptr = 'c';
+
+  assert(*char_ptr=='c');
+  printf("char * -> 'c'       = %c\n", *char_ptr);
+
+  char *str_ptr = malloc(sizeof(char) * 6);
+
+  str_ptr = "Hello";
+
+  assert(strcmp(str_ptr, "Hello") == 0);
+  printf("char * -> 'Hello'   = %s\n", str_ptr);
+
 }
